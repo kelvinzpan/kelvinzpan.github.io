0794138bdb79ba5721bbea68dd6154737a42b957
Kelvin <kelvin.z.pan@gmail.com>
2017-09-03 01:39:16 -0700

    Starter overhaul

--- js/main.js
+++ js/main.js
@@ -1,88 +1,78 @@
-// Clicking the main rectangle will open/close menu, first click will show rest of website
-function menuClick() {
-	var mainMenu = document.getElementById('main-menu');
-	if( mainMenu.style.visibility == 'hidden' ) {
-		mainMenu.style.visibility = 'visible';
-		mainMenu.style.opacity = '1';
-	} else {
-		mainMenu.style.visibility = 'hidden';
-		mainMenu.style.opacity = '0';
-	}
+// Color palette from main.scss
+let primary_dark = '#252525';
+let secondary_dark = '#333333';
+let primary_light = '#ffffff';
 
-	document.getElementById('content').style.display = 'block';
-}
+let grad1 = '#FFF6B7';
+let grad2 = '#FDD1A8';
+let grad3 = '#FBAD99';
+let grad4 = '#F9898A';
+let grad5 = '#F7657B';
+let grad6 = '#F6416C';
 
-// When the user scrolls down from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// Lazy approach to keeping menu colors
+// CHANGE TO WHEN SCROLLING PAST ANCHORS
+// function navAboutActive()
+// {
+//     let about = document.getElementById('navAbout');
+//     let code = document.getElementById('navCode');
+//     let art = document.getElementById('navArt');
+//     let contact = document.getElementById('navContact');
 
-function scrollFunction() {
-	var h = $(window).height() / 2;
-    if (document.body.scrollTop > h || document.documentElement.scrollTop > h) {
-        document.getElementById("top-button").style.visibility = 'visible';
-        document.getElementById("top-button").style.opacity = '1';
-    } else {
-        document.getElementById("top-button").style.visibility = 'hidden';
-        document.getElementById("top-button").style.opacity = '0';
-    }
+//     about.style.background = grad2;
+//     about.style.color = primary_dark;
+//     code.style.background = secondary_dark;
+//     code.style.color = primary_light;
+//     art.style.background = secondary_dark;
+//     art.style.color = primary_light;
+//     contact.style.background = secondary_dark;
+//     contact.style.color = primary_light;
+// }
+// function navCodeActive()
+// {
+//     let about = document.getElementById('navAbout');
+//     let code = document.getElementById('navCode');
+//     let art = document.getElementById('navArt');
+//     let contact = document.getElementById('navContact');
 
-    // Add functionality for showing highlighting code divs when scrolled to
-    c1 = document.getElementById("code-1");
-    c2 = document.getElementById("code-2");
+//     about.style.background = secondary_dark;
+//     about.style.color = primary_light;
+//     code.style.background = grad3;
+//     code.style.color = primary_dark;
+//     art.style.background = secondary_dark;
+//     art.style.color = primary_light;
+//     contact.style.background = secondary_dark;
+//     contact.style.color = primary_light;
+// }
+// function navArtActive()
+// {
+//     let about = document.getElementById('navAbout');
+//     let code = document.getElementById('navCode');
+//     let art = document.getElementById('navArt');
+//     let contact = document.getElementById('navContact');
 
-    if (document.body.scrollTop > c1.scrollTop + h*2 + c1.offsetHeight/2 ||
-    	document.documentElement.scrollTop > c1.scrollTop + h*2 + c1.offsetHeight/2 ) {
-        c1.style.opacity = '1';
-    } else {
-        c1.style.opacity = '0.1';
-    }
-}
+//     about.style.background = secondary_dark;
+//     about.style.color = primary_light;
+//     code.style.background = secondary_dark;
+//     code.style.color = primary_light;
+//     art.style.background = grad4;
+//     art.style.color = primary_dark;
+//     contact.style.background = secondary_dark;
+//     contact.style.color = primary_light;
+// }
+// function navContactActive()
+// {
+//     let about = document.getElementById('navAbout');
+//     let code = document.getElementById('navCode');
+//     let art = document.getElementById('navArt');
+//     let contact = document.getElementById('navContact');
 
-// When user hovers a menu item, front background changes
-var img_main = document.getElementById('img-main');
-var img_abt = document.getElementById('img-abt');
-var img_prg = document.getElementById('img-prg');
-var img_drw = document.getElementById('img-drw');
-var img_ctt = document.getElementById('img-ctt');
-
-function bgMain() {
-	img_drw.style.height = $(window).height();
-	img_main.style.visibility = 'visible';
-    img_main.style.opacity = '.1';
-}
-
-function bgAbout() {
-	img_drw.style.height = $(window).height();
-	img_abt.style.visibility = 'visible';
-    img_abt.style.opacity = '.1';
-} 
-
-function bgProg() {
-	img_drw.style.height = $(window).height();
-	img_prg.style.visibility = 'visible';
-    img_prg.style.opacity = '.1';
-}
-
-function bgDraw() {
-	img_drw.style.height = $(window).height();
-	img_drw.style.visibility = 'visible';
-    img_drw.style.opacity = '.1';
-}
-
-function bgContact() {
-	img_drw.style.height = $(window).height();
-	img_ctt.style.visibility = 'visible';
-    img_ctt.style.opacity = '.1';
-}
-
-function bgDefault() {
-	img_main.style.visibility = 'hidden';
-    img_main.style.opacity = '0';
-    img_abt.style.visibility = 'hidden';
-    img_abt.style.opacity = '0';
-    img_prg.style.visibility = 'hidden';
-    img_prg.style.opacity = '0';
-    img_drw.style.visibility = 'hidden';
-    img_drw.style.opacity = '0';
-    img_ctt.style.visibility = 'hidden';
-    img_ctt.style.opacity = '0';
-}
+//     about.style.background = secondary_dark;
+//     about.style.color = primary_light;
+//     code.style.background = secondary_dark;
+//     code.style.color = primary_light;
+//     art.style.background = secondary_dark;
+//     art.style.color = primary_light;
+//     contact.style.background = grad5;
+//     contact.style.color = primary_dark;
+// }
